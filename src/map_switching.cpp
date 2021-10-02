ca41617ff3c5471e3eebe49f79ea8f2fb213b6da
Ramune6110 <arduino6110@gmail.com>
2021-10-02 10:42:56 -0700

    [add]waypoint_callback

--- src/map_switching.cpp
+++ src/map_switching.cpp
@@ -13,6 +13,8 @@
 #include "tf_lis.h"
 #include <tf/transform_datatypes.h>
 #include <tf/transform_listener.h>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
 
 using namespace std;
 
@@ -23,6 +25,12 @@ vector<nav_msgs::OccupancyGrid> map_array(select_maps);
 std_msgs::Int32 map_num;
 
 geometry_msgs::Pose current_robot_pose;
+
+// 各種map切り替え時のx, y座標
+float switching_waypoint_x;
+float switching_waypoint_y;
+float switching_waypoint_w;
+bool map_chenge_flag[6] = {true, false, false, false, false, false};
 
 // map0
 void map_callback0(const nav_msgs::OccupancyGrid& map_msg) {
@@ -179,12 +187,36 @@ void map_to_baselink_callback(const geometry_msgs::Pose &msg) {
     current_robot_pose.orientation.w = msg.orientation.w;
 }
 
+void waypoint_callback(const visualization_msgs::Marker &msg) {
+    switching_waypoint_x = msg.pose.position.x;
+    switching_waypoint_y = msg.pose.position.y;
+    switching_waypoint_w = msg.pose.orientation.w;
+
+    if (switching_waypoint_x == -24.4127 && switching_waypoint_y == -28.9142 && switching_waypoint_y == -0.999601) {
+        map_chenge_flag[0] = false;
+        map_chenge_flag[1] = true;
+    } else if (switching_waypoint_x == 203.174 && switching_waypoint_y == -82.518 && switching_waypoint_y == -0.721927) {
+        map_chenge_flag[1] = false;
+        map_chenge_flag[2] = true;
+    } else if (switching_waypoint_x == 5.92503 && switching_waypoint_y == 148.57 && switching_waypoint_y == -0.643964) {
+        map_chenge_flag[2] = false;
+        map_chenge_flag[3] = true;
+    } else if (switching_waypoint_x == 14.4161 && switching_waypoint_y == -2.06727 && switching_waypoint_y == -0.999967) {
+        map_chenge_flag[3] = false;
+        map_chenge_flag[4] = true;
+    } else if (switching_waypoint_x == 13.8703 && switching_waypoint_y == 121.256 && switching_waypoint_y == 0.751127) {
+        map_chenge_flag[4] = false;
+        map_chenge_flag[5] = true;
+    }
+}
+
 int main(int argc, char **argv){
     ros::init(argc, argv, "map_swithing");
 
     ros::NodeHandle n;
     ros::Publisher map_pub = n.advertise<nav_msgs::OccupancyGrid>("/map", 10);
     ros::Subscriber map_to_baselink_tf_sub = n.subscribe("/map_to_base_link", 50, map_to_baselink_callback);
+    ros::Subscriber waypoint_sub = n.subscribe("/waypoint", 10, waypoint_callback);
     ros::ServiceClient map_client = n.serviceClient<nav_msgs::SetMap>("set_map");
 
     ros::NodeHandle lSubscriber("");
@@ -204,12 +236,39 @@ int main(int argc, char **argv){
     ros::Rate loop_rate(50);
 
     while (n.ok()) {
+        /*
         map_pub.publish(map_array.at(0));
         map_pub.publish(map_array.at(1));
         map_pub.publish(map_array.at(2));
         map_pub.publish(map_array.at(3));
         map_pub.publish(map_array.at(4));
         map_pub.publish(map_array.at(5));
+        */
+       
+        if (map_chenge_flag[0] == true) 
+        {
+            map_pub.publish(map_array.at(0));
+        } 
+        else if (map_chenge_flag[1] == true) 
+        {
+            map_pub.publish(map_array.at(1));
+        } 
+        else if (map_chenge_flag[2] == true) 
+        {
+            map_pub.publish(map_array.at(2));
+        } 
+        else if (map_chenge_flag[3] == true) 
+        {
+            map_pub.publish(map_array.at(3));
+        } 
+        else if (map_chenge_flag[4] == true) 
+        {
+            map_pub.publish(map_array.at(4));
+        } 
+        else if (map_chenge_flag[5] == true) 
+        {
+            map_pub.publish(map_array.at(5));
+        }
 
         ros::spinOnce();
         loop_rate.sleep();
